23f6b73a7cd5b6b37dd620cb318018bbcf8e537c
Kenta Suzuki <suzuken326@gmail.com>
2012-10-14 20:31:47 +0900

    solved 2.23

--- chapter2/2.2.scm
+++ chapter2/2.2.scm
@@ -195,3 +195,12 @@
 ;Value 17: (1 4 9 16)
 
 ; 2.23
+
+(define (for-each proc items)
+  (cond ((null? items) nil)
+        (else
+          (proc (car items))
+          (for-each proc (cdr items)))))
+
+; (for-each (lambda (x) (newline) (display x))
+;           (list 57 321 88))
