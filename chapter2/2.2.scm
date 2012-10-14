e9d0e8d05d93f531f2d8e4936ee488d4128b0fa7
Kenta Suzuki <suzuken326@gmail.com>
2012-10-14 18:50:04 +0900

    solved 2.20, 2.21, 2.22

--- chapter2/2.2.scm
+++ chapter2/2.2.scm
@@ -101,4 +101,97 @@
 
 ; 2.20
 
+(define (same-parity x . z)
+  (define (iter items results)
+    (if (null? items)
+      results
+      (iter (cdr items)
+            (if (= (remainder (car items) 2)
+              (remainder x 2))
+              (append results (list (car items)))
+            results))))
+  (iter z (list x)))
 
+; (same-parity 1 2 3 4 5 6 7)
+
+; list mapping
+(define nil '())
+
+(define (scale-list items factor)
+  (if (null? items)
+    nil
+    (cons (* (car items) factor)
+          (scale-list (cdr items) factor))))
+
+;(scale-list (list 1 2 3 4 5) 10)
+
+(define (map proc items)
+  (if (null? items)
+    nil
+    (cons (proc (car items))
+          (map proc (cdr items)))))
+
+;(map abs (list -10 2.5 -11.6 17))
+
+;(map (lambda (x) (* x x))
+;     (list 1 2 3 4))
+
+;(define (scale-list items factor)
+;  (map (lambda (x) (* x factor))
+;       items))
+
+; 2.21
+(define (square-list items)
+  (if (null? items)
+    nil
+    (cons (* (car items) (car items))
+          (square-list (cdr items)))))
+
+; (define (square-list items)
+;   (map (lambda (x) (* x x))
+;        items))
+
+; (square-list (list 1 2 3 4))
+
+; 2.22
+
+; (define (square-list items)
+;   (define (iter things answer)
+;     (if (null? things)
+;       answer
+;       (iter (cdr things)
+;             (cons (square (car things))
+;                   answer))))
+;   (iter items nil))
+
+; consでanswerの先頭に導き出した結果を対にしているため
+
+; (square-list (list 1 2 3 4))
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things)
+            (cons answer
+                  (square (car things))))))
+  (iter items nil))
+
+; (square-list (list 1 2 3 4))
+;;Value 14: ((((() . 1) . 4) . 9) . 16)
+; consの後ろはlistにならなければならない。
+; 書きなおすとしたら以下のようにする。
+
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+      answer
+      (iter (cdr things)
+            (append answer
+                  (list (square (car things)))))))
+  (iter items nil))
+
+(square-list (list 1 2 3 4))
+;Value 17: (1 4 9 16)
+
+; 2.23
