ae61989c5fc0bdbbecafee7a19c1abeb5f66d7b9
Kenta Suzuki <ke-suzuki@voyagegroup.com>
2012-11-14 20:01:49 +0900

    solved 2.36

--- chapter2/2.2.scm
+++ chapter2/2.2.scm
@@ -368,3 +368,20 @@
 
 ; (print (count-leaves-acc (list 1 2 3 4 5)))
 ; -> 5
+
+; 2.36
+
+(define (accumulate-n op init seqs)
+  (if (null? (car seqs))
+    nil
+    (cons (accumulate op init (map car seqs))
+          (accumulate-n op init (map cdr seqs)))))
+
+(print (accumulate-n + 0 (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12))))
+; -> (22 26 30)
+
+; (list (list 1 2 3) (list 4 5 6) (list 7 8 9) (list 10 11 12)) から (list 1 4 7 10)をどうつくるか
+; それぞれ先頭の要素をcarで取り出せれば良い
+;
+; (map (lambda (x) (car x)) seqs) -> (map car seqs)
+
