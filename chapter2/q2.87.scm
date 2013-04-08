613d4c5662d3975e3d1e7f9129fec6d08ad0813b
Kenta Suzuki <suzuken326@gmail.com>
2013-04-08 19:19:51 +0900

    add 2.87

---
+++ chapter2/q2.87.scm
@@ -0,0 +1,34 @@
+(load "./s2.5.scm")
+
+; 多項式パッケージに=zero?を追加
+(define (install-=zero?-polynomial-package)
+  (define (=zero?-polynomial n)
+    (eq? (car n) 0))
+  (put '=zero? '(polynomial)
+       (lambda (x) (=zero?-polynomial x)))
+  'done)
+
+; TESTING
+(load "./q2.78.scm")
+(load "./q2.80.scm")
+(install-polynomial-package)
+(install-scheme-number-package)
+(install-=zero?-package)
+(install-=zero?-polynomial-package)
+(define p1 (make-polynomial 'x '((100 0) (1 10))))
+(define p2 (make-polynomial 'x '((100 2) (1 10))))
+; (use slib)
+; (require 'trace)
+; (trace add)
+; (trace type-tag)
+; (print (add p1 p2))
+(test* 
+  "test"
+  (make-polynomial 'x '((100 2) (1 20)))
+  (add (make-polynomial 'x '((100 0) (1 10))) (make-polynomial 'x '((100 2) (1 10)))))
+
+(test* 
+  "test2"
+  (make-polynomial 'x '((2 ,y1) (1 ,y2) (0 ,y3)))
+  (add (make-polynomial 'x '((2 ,y1) (1 ,y2) (0 ,y3)))
+       (make-polynomial 'x '((1 ,y4) (0 ,y5)))))
