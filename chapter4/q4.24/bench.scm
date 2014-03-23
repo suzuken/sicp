ace8489ad5ca145675a47fe27e167f002b16a318
Kenta Suzuki <suzuken326@gmail.com>
2014-03-23 15:59:04 +0900

    階乗計算のベンチ追加

--- chapter4/q4.24/bench.scm
+++ chapter4/q4.24/bench.scm
@@ -22,3 +22,25 @@
 ; ; real   1.516
 ; ; user   1.520
 ; ; sys    0.000
+
+(define (factorial n)
+  (if (= n 1)
+    1
+    (* (factorial (- n 1)) n)))
+(factorial 100000)
+
+; original
+; ;;; M-Eval input:
+; (factorial 100000)
+; ;(time (eval input the-global-environment))
+; ; real   9.286
+; ; user   9.260
+; ; sys    0.030
+
+; analyzed
+; ;;; M-Eval input:
+; (factorial 100000)
+; ;(time (eval input the-global-environment))
+; ; real   9.643
+; ; user   9.600
+; ; sys    0.040
