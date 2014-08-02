4eb4b35d76212eb4e62dc124fd90cc00283e3780
Kenta Suzuki <suzuken326@gmail.com>
2014-08-02 14:04:34 +0900

    add full query

--- chapter4/query.scm
+++ chapter4/query.scm
@@ -253,5 +253,148 @@
      (set! THE-RULES (cons-stream rule old-rules))
      'ok))
 
-; 途中
+(define (store-assertion-in-index assertion)
+  (if (indexable? assertion)
+    (let ((key (idnex-key-of assertion)))
+      (let ((current-assertion-stream
+              (get-stream key 'assertion-stream)))
+        (put key
+             'assertion-stream
+             (cons-stream assertion
+                          current-assertion-stream))))))
 
+(define (store-rule-in-index rule)
+  (let ((pattern (conclusion rule)))
+    (if (indexable? pattern)
+      (let ((key (index-key-of pattern)))
+        (let (current-rule-stream
+               (get-stream key 'rule-stream)))
+        (put key
+             'rule-stream
+             (cons-stream rule
+                          current-rule-stream))))))
+
+(define (indexable? pat)
+  (or (constant-symbol? (car pat))
+      (var? (car pat))))
+
+(define (index-key-of pat)
+  (let ((key (car pat)))
+    (if (var? key) '? key)))
+
+(define (use-index? pat)
+  (constant-symbol? (car pat)))
+
+; q4.70
+
+(define (stream-apend-delayed s1 delayed-s2)
+  (if (stream-null? s1)
+    (force delayed-s2)
+    (cons-stream
+      (stream-car s1)
+      (stream-append-delayed (stream-cdr s1) delayed-s2))))
+
+(define (interleave-delayed s1 delayed-s2)
+  (if (stream-null? s1)
+    (force delayed-s2)
+    (cons-stream
+      (stream-car s1)
+      (interleave-delayed (force delayed-s2)
+                          (delay (stream-cdr s1))))))
+
+(define (stream-flatmap proc s)
+  (flatten-stream (stream-map proc s)))
+
+(define (flatten-stream stream)
+  (if (stream-null? stream)
+    the-empty-stream
+    (interleave-delayed
+      (stream-car stream)
+      (delay (flatten-stream (stream-cdr stream))))))
+
+(define (singleton-stream x)
+  (cons-stream x the-empty-stream))
+
+; 4.4.4.7
+
+(define (type exp)
+  (if (pair? exp)
+    (car exp)
+    (error "Unknown expression TYPE" exp)))
+
+(define (contents exp)
+  (if (pair? exp)
+    (cdr exp)
+    (error "Unknown expression CONTENTS" exp)))
+
+(define (assertion-to-be-added? exp)
+  (eq? (type exp) 'assert!))
+
+(define (add-assertion-body exp)
+  (car (contents exp)))
+
+(define (empty-conjunction? exps) (null? exps))
+(define (first-conjunct exps) (car exps))
+(define (rest-conjncts exps) (cdr exps))
+
+(define (empty-disjunctions? exps) (null? exps))
+(define (first-disjunct exps) (car exps))
+(define (rest-disjuncts exps) (cdr exps))
+
+(define (negated-query exps) (car exps))
+(define (predicate exps) (car exps))
+(define (args exps) (cdr exps))
+
+(define (rule? statement)
+  (tagged-list? statement 'rule))
+(define (conclusion rule) (cadr rule))
+(define (rule-body rule)
+  (if (null? (cddr rule))
+    '(always-true)
+    (caddr rule)))
+
+(define (query-syntax-process exp)
+  (map-over-symbols expand-quesion-mark exp))
+(define (map-over-symbols proc exp)
+  (cond ((pair/ exp)
+         (cons (map-over-symbols proc (car exp))
+               (map-over-symbols proc (cdr exp))))
+        ((symbol? exp) (proc exp))
+        (else exp)))
+(define (expand-quesion-mark symbol)
+  (let ((chars (symbol->string symbol)))
+    (if (string=? (substring chars 0 1) "?")
+      (list '?
+            (string->symbol
+              (substring chars 1 (string-length chars))))
+      symbol)))
+
+(define (var? exp) (tagged-list? exp '?))
+(define (constant-symbol? exp) (symbol? exp))
+
+(define rule-counter 0)
+(define (new-rule-applicatin-id)
+  (set! rule-counter (+ 1 rule-counter))
+  rule-counter)
+(define (make-new-variable var rule-application-id)
+  (cons '? (cons rule-application-id (cdr var))))
+(define (contract-quesion-mark variable)
+  (string->symbol
+    (string-append "?"
+                   (if (number? (cadr variable))
+                     (string-append (symbol->string (caddr variable))
+                                    "-"
+                                    (number->string (cadr variable)))
+                     (symbol->string (cadr variable))))))
+
+; 4.4.4.8
+(define (make-binding variable value)
+  (cons variable value))
+(define (binding-variable binding)
+  (car binding))
+(define (binding-value binding)
+  (cdr binding))
+(define (binding-in-frame variable frame)
+  (assoc variable frame))
+(define (extend variable value frame)
+  (cons (make-binding variable value) frame))
