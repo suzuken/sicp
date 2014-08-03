05a780efd34c7a5312500a226c740d6ae04f1b2c
Kenta Suzuki <suzuken326@gmail.com>
2014-08-03 20:27:26 +0900

    solved q4.63

--- chapter4/q4.63.scm
+++ chapter4/q4.63.scm
@@ -1,24 +1,41 @@
 ; q4.63
+;
+; ref. http://en.wikipedia.org/wiki/Generations_of_Adam
+; CainはAdamの息子 etc.
 (assert! (son Adam Cain))
 (assert! (son Cain Enoch))
 (assert! (son Enoch Irad))
 (assert! (son Irad Mehujael))
 (assert! (son Mehujael Methushael))
 (assert! (son Methushael Lamech))
-(assert! (wife Lamech Ada))
+(assert! (wife Lamech Ada))  ; AdaはLamechの妻
 (assert! (son Ada Jabal))
 (assert! (son Ada Jubal))
 
-; grandsonの規則を作る
-(rule (grandson ?g ?s)
-      (and (son ?f ?s)
-           (son ?g ?f)))
+; 「SがFの息子であり, かつFがGの息子であるなら, SはGの孫[grandson]である」
+(assert! (rule (grandson-of ?g ?s)
+               (and (son-of ?f ?s)
+                    (son-of ?g ?f))))
 
-(rule (son ?m ?s)
-      (and (wife ?m ?w)
-           (son ?w ?s)))
+; 「WがMの妻であり, かつSがWの息子であるなら, SはMの息子である」
+(assert! (rule (son-of ?m ?s)
+               (or (son ?m ?s)
+                   (and (wife ?m ?w)
+                        (son ?w ?s)))))
 
-(assert! (rule (grandson ?g ?s) (and (son ?f ?s) (son ?g ?f))))
-(assert! (rule (son ?m ?s) (and (wife ?m ?w) (son ?w ?s))))
-(grandson Cain ?s)
-(grandson Methushael ?who)
+(grandson-of Cain ?who)
+(grandson-of Methushael ?s)
+
+; 結果
+;;; Query input:
+;;; (grandson-of Cain ?who)
+;;;
+;;; ;;; Query results:
+;;; (grandson-of Cain Irad)
+;;;
+;;; ;;; Query input:
+;;; (grandson-of Methushael ?s)
+;;;
+;;; ;;; Query results:
+;;; (grandson-of Methushael Jubal)
+;;; (grandson-of Methushael Jabal)
