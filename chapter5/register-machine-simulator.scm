084e835e38c5a3d83422a8d34e86205c09e992d1
Kenta Suzuki <suzuken326@gmail.com>
2014-10-13 15:38:04 +0900

    solved 5.9

--- chapter5/register-machine-simulator.scm
+++ chapter5/register-machine-simulator.scm
@@ -309,20 +309,20 @@
         (else
          (error "Unknown expression type -- ASSEMBLE" exp))))
 
-(define (register-exp? exp) (tagged-list? exp 'reg))
+(define (tagged-list? exp tag)
+ (if (pair? exp)
+     (eq? (car exp) tag)
+     false))
 
+(define (register-exp? exp) (tagged-list? exp 'reg))
 
 (define (register-exp-reg exp) (cadr exp))
 
-
 (define (constant-exp? exp) (tagged-list? exp 'const))
-
 
 (define (constant-exp-value exp) (cadr exp))
 
-
 (define (label-exp? exp) (tagged-list? exp 'label))
-
 
 (define (label-exp-label exp) (cadr exp))
 
