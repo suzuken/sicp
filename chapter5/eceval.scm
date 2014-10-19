e8f007352856b392ecd8070b77600b6cbcc99e17
Kenta Suzuki <suzuken326@gmail.com>
2014-10-19 11:30:44 +0900

    積極制御評価器の駆動ループを追加

--- chapter5/eceval.scm
+++ chapter5/eceval.scm
@@ -1,4 +1,92 @@
-; 制御評価器を書き下ろしたもの
+; 積極制御評価器のoperationsと評価器本体
+(define eceval-operations
+  (list
+   (list 'read read)
+
+   (list 'self-evaluating? self-evaluating?)
+   (list 'quoted? quoted?)
+   (list 'text-of-quotation text-of-quotation)
+   (list 'variable? variable?)
+   (list 'assignment? assignment?)
+   (list 'assignment-variable assignment-variable)
+   (list 'assignment-value assignment-value)
+   (list 'definition? definition?)
+   (list 'definition-variable definition-variable)
+   (list 'definition-value definition-value)
+   (list 'lambda? lambda?)
+   (list 'lambda-parameters lambda-parameters)
+   (list 'lambda-body lambda-body)
+   (list 'if? if?)
+   (list 'if-predicate if-predicate)
+   (list 'if-consequent if-consequent)
+   (list 'if-alternative if-alternative)
+   (list 'begin? begin?)
+   (list 'begin-actions begin-actions)
+   (list 'last-exp? last-exp?)
+   (list 'first-exp first-exp)
+   (list 'rest-exps rest-exps)
+   (list 'application? application?)
+   (list 'operator operator)
+   (list 'operands operands)
+   (list 'no-operands? no-operands?)
+   (list 'first-operand first-operand)
+   (list 'rest-operands rest-operands)
+
+   (list 'true? true?)
+   (list 'make-procedure make-procedure)
+   (list 'compound-procedure? compound-procedure?)
+   (list 'procedure-parameters procedure-parameters)
+   (list 'procedure-body procedure-body)
+   (list 'procedure-environment procedure-environment)
+   (list 'extend-environment extend-environment)
+   (list 'lookup-variable-value lookup-variable-value)
+   (list 'set-variable-value! set-variable-value!)
+   (list 'define-variable! define-variable!)
+   (list 'primitive-procedure? primitive-procedure?)
+   (list 'apply-primitive-procedure apply-primitive-procedure)
+   (list 'prompt-for-input prompt-for-input)
+   (list 'announce-output announce-output)
+   (list 'user-print user-print)
+   (list 'empty-arglist empty-arglist)
+   (list 'adjoin-arg adjoin-arg)
+   (list 'last-operand? last-operand?)
+   (list 'no-more-exps? no-more-exps?)
+   (list 'get-global-environment get-global-environment))
+   )
+
+(define eceval
+  (make-machine
+   '(exp env val proc argl continue unev)
+   eceval-operations
+  '(
+read-eval-print-loop
+  (perform (op initialize-stack))
+  (perform
+   (op prompt-for-input) (const ";;; EC-Eval input:"))
+  (assign exp (op read))
+  (assign env (op get-global-environment))
+  (assign continue (label print-result))
+  (goto (label eval-dispatch))
+print-result
+  (perform (op print-stack-statistics))
+  (perform
+   (op announce-output) (const ";;; EC-Eval value:"))
+  (perform (op user-print) (reg val))
+  (goto (label read-eval-print-loop))
+
+unknown-expression-type
+  (assign val (const unknown-expression-type-error))
+  (goto (label signal-error))
+
+unknown-procedure-type
+  (restore continue)
+  (assign val (const unknown-procedure-type-error))
+  (goto (label signal-error))
+
+signal-error
+  (perform (op user-print) (reg val))
+  (goto (label read-eval-print-loop))
+
 eval-dispatch
   (test (op self-evaluating?) (reg exp))
   (branch (label ev-self-eval))
@@ -23,15 +111,12 @@ eval-dispatch
 ev-self-eval
   (assign val (reg exp))
   (goto (reg continue))
-
 ev-variable
   (assign val (op lookup-variable-value) (reg exp) (reg env))
   (goto (reg continue))
-
 ev-quoted
   (assign val (op text-of-quotation) (reg exp))
   (goto (reg continue))
-
 ev-lambda
   (assign unev (op lambda-parameters) (reg exp))
   (assign exp (op lambda-body) (reg exp))
@@ -47,7 +132,6 @@ ev-application
   (assign exp (op operator) (reg exp))
   (assign continue (label ev-appl-did-operator))
   (goto (label eval-dispatch))
-
 ev-appl-did-operator
   (restore unev)
   (restore env)
@@ -56,7 +140,6 @@ ev-appl-did-operator
   (test (op no-operands?) (reg unev))
   (branch (label apply-dispatch))
   (save proc)
-
 ev-appl-operand-loop
   (save argl)
   (assign exp (op first-operand) (reg unev))
@@ -66,7 +149,6 @@ ev-appl-operand-loop
   (save unev)
   (assign continue (label ev-appl-accumulate-arg))
   (goto (label eval-dispatch))
-
 ev-appl-accumulate-arg
   (restore unev)
   (restore env)
@@ -74,7 +156,6 @@ ev-appl-accumulate-arg
   (assign argl (op adjoin-arg) (reg val) (reg argl))
   (assign unev (op rest-operands) (reg unev))
   (goto (label ev-appl-operand-loop))
-
 ev-appl-last-arg
   (assign continue (label ev-appl-accum-last-arg))
   (goto (label eval-dispatch))
@@ -83,7 +164,6 @@ ev-appl-accum-last-arg
   (assign argl (op adjoin-arg) (reg val) (reg argl))
   (restore proc)
   (goto (label apply-dispatch))
-
 apply-dispatch
   (test (op primitive-procedure?) (reg proc))
   (branch (label primitive-apply))
@@ -107,11 +187,11 @@ compound-apply
   (goto (label ev-sequence))
 
 ; s5.4.2
-
 ev-begin
   (assign unev (op begin-actions) (reg exp))
   (save continue)
   (goto (label ev-sequence))
+
 
 ev-sequence
   (assign exp (op first-exp) (reg unev))
@@ -130,6 +210,7 @@ ev-sequence-last-exp
   (restore continue)
   (goto (label eval-dispatch))
 
+; s5.4.3
 ev-if
   (save exp)
   (save env)
@@ -137,7 +218,6 @@ ev-if
   (assign continue (label ev-if-decide))
   (assign exp (op if-predicate) (reg exp))
   (goto (label eval-dispatch))
-
 ev-if-decide
   (restore continue)
   (restore env)
@@ -176,7 +256,6 @@ ev-definition
   (save continue)
   (assign continue (label ev-definition-1))
   (goto (label eval-dispatch))
-
 ev-definition-1
   (restore continue)
   (restore env)
@@ -185,32 +264,4 @@ ev-definition-1
    (op define-variable!) (reg unev) (reg val) (reg env))
   (assign val (const ok))
   (goto (reg continue))
-
-d-eval-print-loop
-  (perform (op initialize-stack))
-  (perform
-   (op prompt-for-input) (const ";;; EC-Eval input:"))
-  (assign exp (op read))
-  (assign env (op get-global-environment))
-  (assign continue (label print-result))
-  (goto (label eval-dispatch))
-
-print-result
-  (perform
-   (op announce-output) (const ";;; EC-Eval value:"))
-  (perform (op user-print) (reg val))
-  (goto (label read-eval-print-loop))
-
-unknown-expression-type
-  (assign val (const unknown-expression-type-error))
-  (goto (label signal-error))
-
-
-unknown-procedure-type
-  (restore continue)
-  (assign val (const unknown-procedure-type-error))
-  (goto (label signal-error))
-
-signal-error
-  (perform (op user-print) (reg val))
-  (goto (label read-eval-print-loop))
+   )))
